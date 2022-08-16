eafceed75b4d3b20c3f58f7b9df5a497c37cde15
Farid0558475484 <faridmh@code.edu.az>
2022-08-16 18:37:55 +0400

    bir commit

---
+++ Bir.Figure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bir.Figure
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
